15d05fdff5388bd474df5a2dfb419710d78ad2d4
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-15 14:44:27 +0530

    ADDED : checking last name of the user

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,20 +2,41 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    static String firstName;
-      public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	static String firstName;
+	static String lastName;
+   public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	firstName();
+	lastName();
 
-        System.out.println("Enter FirstName: ");
-        firstName = scanner.next();
-        if (Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z|A-Z]*$", firstName)) {
-            System.out.println("Valid First Name");
-        } else {
-            System.out.println("Invalid First Name");
-        }
+	}
 
-    }
-}
+      }
+	class static void firstName() {
+		while (true) {
+        	System.out.println("Enter FirstName: ");
+        	firstName = input.nextLine();
+        	if (Pattern.matches("^[A-Z]{1}[a-zA-Z]{2, }$", firstName)) {
+            	System.out.println("Valid First Name");
+        	}
+		else {
+           	System.out.println("Invalid First Name");
+		}
+	    }
+    	}
+	class static void lastName() {
+	 	while (true) {
+		System.out.println("Enter LastName: ");
+        	lastName = input.nextLine();
+        	if (Pattern.matches("^[A-Z]{1}[a-zA-z]{2, }$", lastName)) {
+            	System.out.println("Valid Last Name");
+        	}
+        	else {
+            	System.out.println("Invalid Last Name");
+		}
+	   }
+	}
+	
 
 
 
