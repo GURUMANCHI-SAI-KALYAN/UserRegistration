4fcce5893356407f74daa487652c27c0328f8b36
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-15 15:31:30 +0530

    ADDED CHECK EMAIL BY USER PROGRAM

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,41 +2,48 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	static String firstName;
-	static String lastName;
    public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
-	firstName();
-	lastName();
+        UserRegistration userRegistration = new UserRegistration();
 
+        System.out.println("Welcome to User Registration!");
+        System.out.println("Enter First Name: ");
+        userRegistration.firstName(input.next());
+        System.out.println("Enter Last Name: ");
+        userRegistration.lastName(input.next());
+        System.out.println("Enter Email id: ");
+        userRegistration.email(input.next());
+//        scanner.close();
 	}
 
-      }
-	class static void firstName() {
-		while (true) {
+	public static void firstName( String firstName ) {
         	System.out.println("Enter FirstName: ");
-        	firstName = input.nextLine();
-        	if (Pattern.matches("^[A-Z]{1}[a-zA-Z]{2, }$", firstName)) {
+        	if (Pattern.matches("^[A-Z]{1}[a-zA-Z]*$", firstName)) {
             	System.out.println("Valid First Name");
         	}
 		else {
            	System.out.println("Invalid First Name");
 		}
 	    }
-    	}
-	class static void lastName() {
-	 	while (true) {
-		System.out.println("Enter LastName: ");
-        	lastName = input.nextLine();
-        	if (Pattern.matches("^[A-Z]{1}[a-zA-z]{2, }$", lastName)) {
+	public static void lastName( String lastName ) {
+        	if (Pattern.matches("^[A-Z]{1}[a-zA-z]*$", lastName)) {
             	System.out.println("Valid Last Name");
         	}
         	else {
             	System.out.println("Invalid Last Name");
 		}
 	   }
-	}
 	
+	 public static void email(String email) { 
+		System.out.println("Enter Email: "); 
+                if (Pattern.matches("^[A-Za-Z]+([.\\+_-#@][a-zA-z])*@[0-9a-zA-Z]+.[a-z]{2,4}([.][a-zA-Z]{2, }*$", email)) {
+                System.out.println("Valid Email");
+                }
+                else {
+                System.out.println("Invalid Email");
+                }
+           }
+    }
 
 
 
